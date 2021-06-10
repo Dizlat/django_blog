e50ea5409f15036dda1a7d25078b41de987a5f03
Dizlat <dizlat.ur@gmail.com>
2021-06-10 16:52:39 +0600

    url

--- main/urls.py
+++ main/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('posts/details/<int:pk>/', views.PostDetailsView.as_view(),
          name='post-details'),
     path('post/create/new/', views.CreateNewPostView.as_view(), name='create-post'),
-    path('post/update/', views.EditPostView.as_view(), name='edit-post'),
+    path('post/update/<int:pk>/', views.EditPostView.as_view(), name='edit-post'),
     path('post/delete/<int:pk>/', views.DeletePostView.as_view(), name='delete-post'),
 ]
